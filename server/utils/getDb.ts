d1095b9c6fed7b2ef5ade41f1da3e4a97d11fb38
fomnn <fathurrahmann0803@gmail.com>
2025-10-27 22:00:29 +0700

    feat: update database reference to ihka_jabar_v2

--- server/utils/getDb.ts
+++ server/utils/getDb.ts
@@ -3,6 +3,6 @@ import * as schema from '@@/server/db/schema'
 import { drizzle } from 'drizzle-orm/d1'
 
 export function getDb(event: any) {
-  const d1Db = event.context.cloudflare.env.DB as D1Database
+  const d1Db = event.context.cloudflare.env.ihka_jabar_v2 as D1Database
   return drizzle(d1Db, { schema })
 }
